192e7f7fd4d2316e13c6ebd8b9b4689c04d6c40c
OriginalAuthority <luke@telepedia.net>
2024-07-23 18:46:23 +0100

    change container name to avoid collisions with SocialProfile

--- includes/Avatar/UploadAvatar.php
+++ includes/Avatar/UploadAvatar.php
@@ -11,242 +11,242 @@ use UploadFromFile;
 
 class UploadAvatar extends UploadFromFile {
 
-	public $mExtension;
+    public $mExtension;
 
-	public function performUpload($comment, $pageText, $watch, $user, $tags = [], ?string $watchlistExpiry = null) {
-		$wgAvatarKey = 'avatar';
+    public function performUpload($comment, $pageText, $watch, $user, $tags = [], ?string $watchlistExpiry = null) {
+        $wgAvatarKey = 'avatar';
 
-		$cacheCon = ObjectCache::getLocalClusterInstance();
+        $cacheCon = ObjectCache::getLocalClusterInstance();
 
-		/**
-		 * The file is empty, return a fatal
-		 */
-		$imageInfo = getimagesize($this->mTempPath);
-		if (empty($imageInfo[2])) {
-			return Status::newFatal('empty-file');
-		}
+        /**
+         * The file is empty, return a fatal
+         */
+        $imageInfo = getimagesize($this->mTempPath);
+        if (empty($imageInfo[2])) {
+            return Status::newFatal('empty-file');
+        }
 
-		switch ($imageInfo[2]) {
-			case 1:
-				$ext = 'gif';
-				break;
-			case 2:
-				$ext = 'jpg';
-				break;
-			case 3:
-				$ext = 'png';
-				break;
-			default:
-				return Status::newFatal('filetype-banned');
-		}
+        switch ($imageInfo[2]) {
+            case 1:
+                $ext = 'gif';
+                break;
+            case 2:
+                $ext = 'jpg';
+                break;
+            case 3:
+                $ext = 'png';
+                break;
+            default:
+                return Status::newFatal('filetype-banned');
+        }
 
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('UserProfileV2');
+        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('UserProfileV2');
 
-		if ($config->get('UserProfileV2UseGlobalAvatars')) {
-			$caUser = CentralAuthUser::getPrimaryInstanceByName($user->getName());
-			$userId = $caUser->getId();
-		} else {
-			$userId = $user->getId();
-		}
+        if ($config->get('UserProfileV2UseGlobalAvatars')) {
+            $caUser = CentralAuthUser::getPrimaryInstanceByName($user->getName());
+            $userId = $caUser->getId();
+        } else {
+            $userId = $user->getId();
+        }
 
 
-		$userProfileAvatar = new UserProfileV2Avatar($userId);
+        $userProfileAvatar = new UserProfileV2Avatar($userId);
 
-		$this->createThumbnail($this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $userId, 138);
+        $this->createThumbnail($this->mTempPath, $imageInfo, $wgAvatarKey . '_' . $userId, 138);
 
-		$extensions = ['jpg', 'gif', 'png', 'jpeg', 'webp'];
+        $extensions = ['jpg', 'gif', 'png', 'jpeg', 'webp'];
 
-		foreach ($extensions as $fileExt) {
-			if ($ext != $fileExt) {
-				$filePath = wfTempDir() . "/{$wgAvatarKey}_{$userId}.{$fileExt}";
-				if (is_file($filePath)) {
-					unlink($filePath);
-				}
-			}
-		}
+        foreach ($extensions as $fileExt) {
+            if ($ext != $fileExt) {
+                $filePath = wfTempDir() . "/{$wgAvatarKey}_{$userId}.{$fileExt}";
+                if (is_file($filePath)) {
+                    unlink($filePath);
+                }
+            }
+        }
 
-		$avatarBackend = new UserProfileV2AvatarBackend('avatars');
+        $avatarBackend = new UserProfileV2AvatarBackend('upv2avatars');
 
-		foreach (['gif', 'jpg', 'jpeg', 'png'] as $fileExtension) {
-			if ($fileExtension === $ext) {
-				// Our brand new avatar; skip over it in order to _not_ delete it, obviously
-			} else {
-				if ($avatarBackend->fileExists($wgAvatarKey . '_', $userId, $fileExtension)) {
-					$avatarBackend->getFileBackend()->quickDelete([
-						'src' => $avatarBackend->getPath($wgAvatarKey . '_', $userId, $fileExtension)
-					]);
-				}
-			}
-		}
+        foreach (['gif', 'jpg', 'jpeg', 'png'] as $fileExtension) {
+            if ($fileExtension === $ext) {
+                // Our brand new avatar; skip over it in order to _not_ delete it, obviously
+            } else {
+                if ($avatarBackend->fileExists($wgAvatarKey . '_', $userId, $fileExtension)) {
+                    $avatarBackend->getFileBackend()->quickDelete([
+                        'src' => $avatarBackend->getPath($wgAvatarKey . '_', $userId, $fileExtension)
+                    ]);
+                }
+            }
+        }
 
-		$key = $cacheCon->makeKey('user', 'userprofilev2', 'avatar', $userId);
-		$cacheCon->delete($key);
+        $key = $cacheCon->makeKey('user', 'userprofilev2', 'avatar', $userId);
+        $cacheCon->delete($key);
 
-		$this->mExtension = $ext;
+        $this->mExtension = $ext;
 
-		return Status::newGood();
-	}
+        return Status::newGood();
+    }
 
-	public function checkWarnings($user = null) {
-		return [];
-	}
+    public function checkWarnings($user = null) {
+        return [];
+    }
 
-	private function createThumbnail($imageSrc, $imageInfo, $imgDest, $thumbWidth) {
-		$backend = new UserProfileV2AvatarBackend('avatars');
-		$fname = $backend->getContainerStoragePath();
+    private function createThumbnail($imageSrc, $imageInfo, $imgDest, $thumbWidth) {
+        $backend = new UserProfileV2AvatarBackend('upv2avatars');
+        $fname = $backend->getContainerStoragePath();
 
-		$fileBackend = $backend->getFileBackend();
-		$status = $fileBackend->prepare(['dir' => $fname]);
+        $fileBackend = $backend->getFileBackend();
+        $status = $fileBackend->prepare(['dir' => $fname]);
 
-		if (!$status->isOK()) {
-			throw new Exception(
-				wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
-			);
-		}
+        if (!$status->isOK()) {
+            throw new Exception(
+                wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
+            );
+        }
 
-		// shamefully copied from SocialProfile
+        // shamefully copied from SocialProfile
 
-		global $wgUseImageMagick, $wgImageMagickConvertCommand;
+        global $wgUseImageMagick, $wgImageMagickConvertCommand;
 
-		if ($wgUseImageMagick) { // ImageMagick is enabled
-			[$origWidth, $origHeight, $typeCode] = $imageInfo;
+        if ($wgUseImageMagick) { // ImageMagick is enabled
+            [$origWidth, $origHeight, $typeCode] = $imageInfo;
 
-			if ($origWidth < $thumbWidth) {
-				$thumbWidth = $origWidth;
-			}
-			$thumbHeight = ($thumbWidth * $origHeight / $origWidth);
-			$border = ' -bordercolor white  -border  0x';
-			if ($thumbHeight < $thumbWidth) {
-				$border = ' -bordercolor white  -border  0x' . (($thumbWidth - $thumbHeight) / 2);
-			}
-			if ($typeCode == 2) {
-				exec(
-					$wgImageMagickConvertCommand . ' -size ' . $thumbWidth . 'x' . $thumbWidth .
-					' -resize ' . $thumbWidth . ' -crop ' . $thumbWidth . 'x' .
-					$thumbWidth . '+0+0   -quality 100 ' . $border . ' ' .
-					$imageSrc . ' ' . wfTempDir() . '/' . $imgDest . '.jpg'
-				);
+            if ($origWidth < $thumbWidth) {
+                $thumbWidth = $origWidth;
+            }
+            $thumbHeight = ($thumbWidth * $origHeight / $origWidth);
+            $border = ' -bordercolor white  -border  0x';
+            if ($thumbHeight < $thumbWidth) {
+                $border = ' -bordercolor white  -border  0x' . (($thumbWidth - $thumbHeight) / 2);
+            }
+            if ($typeCode == 2) {
+                exec(
+                    $wgImageMagickConvertCommand . ' -size ' . $thumbWidth . 'x' . $thumbWidth .
+                    ' -resize ' . $thumbWidth . ' -crop ' . $thumbWidth . 'x' .
+                    $thumbWidth . '+0+0   -quality 100 ' . $border . ' ' .
+                    $imageSrc . ' ' . wfTempDir() . '/' . $imgDest . '.jpg'
+                );
 
-				$status = $fileBackend->quickStore([
-					'src' => wfTempDir() . '/' . $imgDest . '.jpg',
-					'dst' => $fname . '/' . $imgDest . '.jpg'
-				]);
+                $status = $fileBackend->quickStore([
+                    'src' => wfTempDir() . '/' . $imgDest . '.jpg',
+                    'dst' => $fname . '/' . $imgDest . '.jpg'
+                ]);
 
-				if (!$status->isOK()) {
-					throw new Exception(
-						wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
-					);
-				}
-			}
-			if ($typeCode == 1) {
-				exec(
-					$wgImageMagickConvertCommand . ' -size ' . $thumbWidth . 'x' . $thumbWidth .
-					' -resize ' . $thumbWidth . ' -crop ' . $thumbWidth . 'x' .
-					$thumbWidth . '+0+0 ' . $imageSrc . ' ' . $border . ' ' .
-					wfTempDir() . '/' . $imgDest . '.gif'
-				);
+                if (!$status->isOK()) {
+                    throw new Exception(
+                        wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
+                    );
+                }
+            }
+            if ($typeCode == 1) {
+                exec(
+                    $wgImageMagickConvertCommand . ' -size ' . $thumbWidth . 'x' . $thumbWidth .
+                    ' -resize ' . $thumbWidth . ' -crop ' . $thumbWidth . 'x' .
+                    $thumbWidth . '+0+0 ' . $imageSrc . ' ' . $border . ' ' .
+                    wfTempDir() . '/' . $imgDest . '.gif'
+                );
 
-				$status = $fileBackend->quickStore([
-					'src' => wfTempDir() . '/' . $imgDest . '.gif',
-					'dst' => $fname . '/' . $imgDest . '.gif'
-				]);
+                $status = $fileBackend->quickStore([
+                    'src' => wfTempDir() . '/' . $imgDest . '.gif',
+                    'dst' => $fname . '/' . $imgDest . '.gif'
+                ]);
 
-				if (!$status->isOK()) {
-					throw new Exception(
-						wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
-					);
-				}
-			}
-			if ($typeCode == 3) {
-				exec(
-					$wgImageMagickConvertCommand . ' -size ' . $thumbWidth . 'x' . $thumbWidth .
-					' -resize ' . $thumbWidth . ' -crop ' . $thumbWidth . 'x' .
-					$thumbWidth . '+0+0 ' . $imageSrc . ' ' .
-					wfTempDir() . '/' . $imgDest . '.png'
-				);
+                if (!$status->isOK()) {
+                    throw new Exception(
+                        wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
+                    );
+                }
+            }
+            if ($typeCode == 3) {
+                exec(
+                    $wgImageMagickConvertCommand . ' -size ' . $thumbWidth . 'x' . $thumbWidth .
+                    ' -resize ' . $thumbWidth . ' -crop ' . $thumbWidth . 'x' .
+                    $thumbWidth . '+0+0 ' . $imageSrc . ' ' .
+                    wfTempDir() . '/' . $imgDest . '.png'
+                );
 
-				$status = $fileBackend->quickStore([
-					'src' => wfTempDir() . '/' . $imgDest . '.png',
-					'dst' => $fname . '/' . $imgDest . '.png'
-				]);
+                $status = $fileBackend->quickStore([
+                    'src' => wfTempDir() . '/' . $imgDest . '.png',
+                    'dst' => $fname . '/' . $imgDest . '.png'
+                ]);
 
-				if (!$status->isOK()) {
-					throw new Exception(
-						wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
-					);
-				}
-			}
-		} else { // ImageMagick is not enabled, so fall back to PHP's GD library
-			// Get the image size, used in calculations later.
-			[$origWidth, $origHeight, $typeCode] = getimagesize($imageSrc);
+                if (!$status->isOK()) {
+                    throw new Exception(
+                        wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
+                    );
+                }
+            }
+        } else { // ImageMagick is not enabled, so fall back to PHP's GD library
+            // Get the image size, used in calculations later.
+            [$origWidth, $origHeight, $typeCode] = getimagesize($imageSrc);
 
-			$fullImage = '';
-			$ext = '';
+            $fullImage = '';
+            $ext = '';
 
-			switch ($typeCode) {
-				case '1':
-					$fullImage = imagecreatefromgif($imageSrc);
-					$ext = 'gif';
-					break;
-				case '2':
-					$fullImage = imagecreatefromjpeg($imageSrc);
-					$ext = 'jpg';
-					break;
-				case '3':
-					$fullImage = imagecreatefrompng($imageSrc);
-					$ext = 'png';
-					break;
-			}
+            switch ($typeCode) {
+                case '1':
+                    $fullImage = imagecreatefromgif($imageSrc);
+                    $ext = 'gif';
+                    break;
+                case '2':
+                    $fullImage = imagecreatefromjpeg($imageSrc);
+                    $ext = 'jpg';
+                    break;
+                case '3':
+                    $fullImage = imagecreatefrompng($imageSrc);
+                    $ext = 'png';
+                    break;
+            }
 
-			$scale = ($thumbWidth / $origWidth);
+            $scale = ($thumbWidth / $origWidth);
 
-			// Create our thumbnail size, so we can resize to this, and save it.
-			$tnImage = imagecreatetruecolor(
-				$origWidth * $scale,
-				$origHeight * $scale
-			);
+            // Create our thumbnail size, so we can resize to this, and save it.
+            $tnImage = imagecreatetruecolor(
+                $origWidth * $scale,
+                $origHeight * $scale
+            );
 
-			// Resize the image.
-			imagecopyresampled(
-				$tnImage,
-				$fullImage,
-				0, 0, 0, 0,
-				$origWidth * $scale,
-				$origHeight * $scale,
-				$origWidth,
-				$origHeight
-			);
+            // Resize the image.
+            imagecopyresampled(
+                $tnImage,
+                $fullImage,
+                0, 0, 0, 0,
+                $origWidth * $scale,
+                $origHeight * $scale,
+                $origWidth,
+                $origHeight
+            );
 
-			// Create a new image thumbnail.
-			if ($typeCode == 1) {
-				imagegif($tnImage, $imageSrc);
-			} elseif ($typeCode == 2) {
-				imagejpeg($tnImage, $imageSrc);
-			} elseif ($typeCode == 3) {
-				imagepng($tnImage, $imageSrc);
-			}
+            // Create a new image thumbnail.
+            if ($typeCode == 1) {
+                imagegif($tnImage, $imageSrc);
+            } elseif ($typeCode == 2) {
+                imagejpeg($tnImage, $imageSrc);
+            } elseif ($typeCode == 3) {
+                imagepng($tnImage, $imageSrc);
+            }
 
-			// Clean up.
-			imagedestroy($fullImage);
-			imagedestroy($tnImage);
+            // Clean up.
+            imagedestroy($fullImage);
+            imagedestroy($tnImage);
 
-			// Copy the thumb
-			copy(
-				$imageSrc,
-				wfTempDir() . '/' . $imgDest . '.' . $ext
-			);
+            // Copy the thumb
+            copy(
+                $imageSrc,
+                wfTempDir() . '/' . $imgDest . '.' . $ext
+            );
 
-			$status = $fileBackend->quickStore([
-				'src' => wfTempDir() . '/' . $imgDest . '.' . $ext,
-				'dst' => $fname . '/' . $imgDest . '.' . $ext
-			]);
+            $status = $fileBackend->quickStore([
+                'src' => wfTempDir() . '/' . $imgDest . '.' . $ext,
+                'dst' => $fname . '/' . $imgDest . '.' . $ext
+            ]);
 
-			if (!$status->isOK()) {
-				throw new Exception(
-					wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
-				);
-			}
-		}
-	}
+            if (!$status->isOK()) {
+                throw new Exception(
+                    wfMessage('backend-fail-internal', Status::wrap($status)->getWikitext())
+                );
+            }
+        }
+    }
 
 }
--- includes/Avatar/UserProfileV2Avatar.php
+++ includes/Avatar/UserProfileV2Avatar.php
@@ -12,121 +12,121 @@ use StatusValue;
 
 class UserProfileV2Avatar {
 
-	private int $userId;
+    private int $userId;
 
-	public function __construct(int $userId) {
-		$this->userId = $userId;
-	}
+    public function __construct(int $userId) {
+        $this->userId = $userId;
+    }
 
-	/**
-	 * Upload a default avatar
-	 * @return \StatusValue
-	 */
-	private function uploadDefaultAvatar(): StatusValue {
-		$backend = new UserProfileV2AvatarBackend('avatars');
-		return $backend->getFileBackend()->quickStore([
-			'src' => __DIR__ . '/../../resources/avatars/default.jpg',
-			'dst' => $backend->getContainerStoragePath() . '/default.jpg',
-		]);
-	}
+    /**
+     * Upload a default avatar
+     * @return \StatusValue
+     */
+    private function uploadDefaultAvatar(): StatusValue {
+        $backend = new UserProfileV2AvatarBackend('upv2avatars');
+        return $backend->getFileBackend()->quickStore([
+            'src' => __DIR__ . '/../../resources/avatars/default.jpg',
+            'dst' => $backend->getContainerStoragePath() . '/default.jpg',
+        ]);
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getAvatarImage(): string {
-		$wgAvatarKey = 'avatar';
+    /**
+     * @return string
+     */
+    public function getAvatarImage(): string {
+        $wgAvatarKey = 'avatar';
 
-		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('UserProfileV2');
+        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('UserProfileV2');
 
-		$globalAvatars = $config->get('UserProfileV2UseGlobalAvatars');
+        $globalAvatars = $config->get('UserProfileV2UseGlobalAvatars');
 
-		$cache = ObjectCache::getLocalClusterInstance();
+        $cache = ObjectCache::getLocalClusterInstance();
 
-		if ($globalAvatars && ExtensionRegistry::getInstance()->isLoaded('CentralAuth')) {
+        if ($globalAvatars && ExtensionRegistry::getInstance()->isLoaded('CentralAuth')) {
 
-			$centralAuthUser = new CentralAuthUser(User::newFromId($this->userId)->getName());
+            $centralAuthUser = new CentralAuthUser(User::newFromId($this->userId)->getName());
 
-			$this->userId = $centralAuthUser->getId(); // overwrite the userId if we're using global avatars
+            $this->userId = $centralAuthUser->getId(); // overwrite the userId if we're using global avatars
 
-			$key = $cache->makeGlobalKey('user', 'userprofilev2', 'avatar', $this->userId);
-		} else {
-			$key = $cache->makeKey('user', 'userprofilev2', 'avatar', $this->userId);
-		}
-		$data = $cache->get($key);
+            $key = $cache->makeGlobalKey('user', 'userprofilev2', 'avatar', $this->userId);
+        } else {
+            $key = $cache->makeKey('user', 'userprofilev2', 'avatar', $this->userId);
+        }
+        $data = $cache->get($key);
 
-		if ($data) {
-			$avatarFilename = $data;
-			return $avatarFilename;
-		} else {
+        if ($data) {
+            $avatarFilename = $data;
+            return $avatarFilename;
+        } else {
 
-			if (!$this->defaultAvatarExists()) {
-				$this->uploadDefaultAvatar();
-			}
+            if (!$this->defaultAvatarExists()) {
+                $this->uploadDefaultAvatar();
+            }
 
-			$avatar_filename = 'default.jpg';
+            $avatar_filename = 'default.jpg';
 
-			$backend = new UserProfileV2AvatarBackend('avatars');
-			$extensions = ['png', 'gif', 'jpg', 'jpeg'];
-			foreach ($extensions as $ext) {
-				if ($backend->fileExists($wgAvatarKey . '_', $this->userId, $ext)) {
-					$avatar_filename = $backend->getFileName(
-						$wgAvatarKey . '_', $this->userId, $ext
-					);
+            $backend = new UserProfileV2AvatarBackend('upv2avatars');
+            $extensions = ['png', 'gif', 'jpg', 'jpeg'];
+            foreach ($extensions as $ext) {
+                if ($backend->fileExists($wgAvatarKey . '_', $this->userId, $ext)) {
+                    $avatar_filename = $backend->getFileName(
+                        $wgAvatarKey . '_', $this->userId, $ext
+                    );
 
-					$avatar_filename .= '?r=' . $backend->getFileBackend()->getFileStat([
-							'src' => $backend->getContainerStoragePath() . '/' . $avatar_filename
-						])['mtime'];
+                    $avatar_filename .= '?r=' . $backend->getFileBackend()->getFileStat([
+                            'src' => $backend->getContainerStoragePath() . '/' . $avatar_filename
+                        ])['mtime'];
 
-					break;
-				}
-			}
-			$cache->set($key, $avatar_filename, 60 * 60 * 24); // cache for 24 hours
-		}
+                    break;
+                }
+            }
+            $cache->set($key, $avatar_filename, 60 * 60 * 24); // cache for 24 hours
+        }
 
-		return $avatar_filename;
-	}
+        return $avatar_filename;
+    }
 
-	/**
-	 * @return mixed|string
-	 */
-	public function getAvatarUrl($extraParams = []) {
-		$backend = new UserProfileV2AvatarBackend('avatars');
+    /**
+     * @return mixed|string
+     */
+    public function getAvatarUrl($extraParams = []) {
+        $backend = new UserProfileV2AvatarBackend('upv2avatars');
 
-		$url = $backend->getFileHttpUrlFromName($this->getAvatarImage());
+        $url = $backend->getFileHttpUrlFromName($this->getAvatarImage());
 
-		if (isset($extraParams['raw']) && $extraParams['raw'] === true) {
-			return $url;
-		}
+        if (isset($extraParams['raw']) && $extraParams['raw'] === true) {
+            return $url;
+        }
 
-		$defaultParams = [
-			'src' => $url,
-			'border' => '0',
-			'class' => 'mw-userprofilev2-avatar'
-		];
+        $defaultParams = [
+            'src' => $url,
+            'border' => '0',
+            'class' => 'mw-userprofilev2-avatar'
+        ];
 
-		$params = array_merge($extraParams, $defaultParams);
+        $params = array_merge($extraParams, $defaultParams);
 
-		return Html::element('img', $params, '');
-	}
+        return Html::element('img', $params, '');
+    }
 
-	/**
-	 * Check if there is a default avatar image with the supplied $size.
-	 *
-	 * @param string $size Avatar image size
-	 * @return bool|null Returns null on failure
-	 */
-	private function defaultAvatarExists(): bool {
-		$backend = new UserProfileV2AvatarBackend('avatars');
-		return $backend->getFileBackend()->fileExists([
-			'src' => $backend->getContainerStoragePath() . '/default.jpg',
-		]);
-	}
+    /**
+     * Check if there is a default avatar image with the supplied $size.
+     *
+     * @param string $size Avatar image size
+     * @return bool|null Returns null on failure
+     */
+    private function defaultAvatarExists(): bool {
+        $backend = new UserProfileV2AvatarBackend('upv2avatars');
+        return $backend->getFileBackend()->fileExists([
+            'src' => $backend->getContainerStoragePath() . '/default.jpg',
+        ]);
+    }
 
-	/**
-	 * Get a string representation of this avatar
-	 * @return mixed|string
-	 */
-	public function __toString() {
-		return $this->getAvatarURL();
-	}
+    /**
+     * Get a string representation of this avatar
+     * @return mixed|string
+     */
+    public function __toString() {
+        return $this->getAvatarURL();
+    }
 }
