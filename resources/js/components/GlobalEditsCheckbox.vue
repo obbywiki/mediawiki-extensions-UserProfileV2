865c4a06d2d9990cfb105fe80b6af1c26ec2eb23
OriginalAuthority <luke@telepedia.net>
2024-12-22 23:36:53 +0000

    Fix these checkboxes to work properly

--- resources/js/components/GlobalEditsCheckbox.vue
+++ resources/js/components/GlobalEditsCheckbox.vue
@@ -1,35 +1,32 @@
 <template>
-	<cdx-field :is-fieldset="true">
-		<cdx-checkbox
-			v-model="globalEditsInput"
-		></cdx-checkbox>
-		<template #label>
-			{{ $i18n('userprofilev2-showglobaleditcount') }}
-		</template>
-		<template #help-text>
-			{{ $i18n('userprofilev2-showglobaleditcounthelp') }}
-		</template>
+	<cdx-field>
+		<cdx-checkbox v-model="globalEditsInput">
+			{{ $i18n( "userprofilev2-showglobaleditcount" ) }}
+			<template #description>
+				{{ $i18n( "userprofilev2-showglobaleditcounthelp" ) }}
+			</template>
+		</cdx-checkbox>
 	</cdx-field>
 </template>
 
 <script>
 
-const {defineComponent, ref, computed} = require('vue');
-const {CdxField, CdxCheckbox} = require('@wikimedia/codex');
+const { defineComponent, ref, computed } = require( "vue" );
+const { CdxField, CdxCheckbox } = require( "@wikimedia/codex" );
 
-module.exports = defineComponent({
-	name: 'GlobalEditsCheckbox',
+module.exports = defineComponent( {
+	name: "GlobalEditsCheckbox",
 	components: {
 		CdxField,
 		CdxCheckbox
 	},
 	setup() {
-		const globalEditsInput = ref('');
+		const globalEditsInput = ref( "" );
 		return {
-			globalEditsInput,
+			globalEditsInput
 		};
 	}
-});
+} );
 
 </script>
 
--- resources/js/components/GlobalGroupsCheckbox.vue
+++ resources/js/components/GlobalGroupsCheckbox.vue
@@ -1,35 +1,32 @@
 <template>
-	<cdx-field :is-fieldset="true">
-		<cdx-checkbox
-			v-model="globalGroupsInput"
-		></cdx-checkbox>
-		<template #label>
-			{{ $i18n('userprofilev2-showglobalgroups') }}
-		</template>
-		<template #help-text>
-			{{ $i18n('userprofilev2-showglobalgroupshelp') }}
-		</template>
+	<cdx-field>
+		<cdx-checkbox v-model="globalGroupsInput">
+			{{ $i18n( "userprofilev2-showglobalgroups" ) }}
+			<template #description>
+				{{ $i18n( "userprofilev2-showglobalgroupshelp" ) }}
+			</template>
+		</cdx-checkbox>
 	</cdx-field>
 </template>
 
 <script>
 
-const {defineComponent, ref, computed} = require('vue');
-const {CdxField, CdxCheckbox} = require('@wikimedia/codex');
+const { defineComponent, ref, computed } = require( "vue" );
+const { CdxField, CdxCheckbox } = require( "@wikimedia/codex" );
 
-module.exports = defineComponent({
-	name: 'GlobalGroupsCheckbox',
+module.exports = defineComponent( {
+	name: "GlobalGroupsCheckbox",
 	components: {
 		CdxField,
 		CdxCheckbox
 	},
 	setup() {
-		const globalGroupsInput = ref('');
+		const globalGroupsInput = ref( "" );
 		return {
-			globalGroupsInput,
+			globalGroupsInput
 		};
 	}
-});
+} );
 
 </script>
 
